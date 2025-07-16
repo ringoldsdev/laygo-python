32998a8903d71b05c1d03b865a31590c87b4adc3
ringoldsdev <ringolds@lescinskis.com>
2025-07-16 18:11:05 +0000

    chore: bump version

--- laygo/__init__.py
+++ laygo/__init__.py
@@ -2,7 +2,7 @@
 Laygo - A lightweight Python library for building resilient, in-memory data pipelines
 """
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 from .errors import ErrorHandler
 from .helpers import PipelineContext
